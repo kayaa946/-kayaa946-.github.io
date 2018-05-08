af7683c12cbd3d70494e31da46a47e6c69ce2c97
Yaren Kaya <yarenkaya@Yarens-Air-3.fios-router.home>
2018-05-08 09:56:26 -0400

    dream vacation

--- main-dv.js
+++ main-dv.js
@@ -2,15 +2,38 @@ var date = new Date();
 
 function updateTime() {
   date = new Date();
+   // Ratio is: unit of time / 360 degrees//
   
+  // Seconds
   var seconds = date.getSeconds();
-  
-  // Ratio is: unit of time / 360 degrees
   var secondsRatio = 60/360;
   var secondsDegrees = seconds / secondsRatio;
   
-  $('.circle15').css('transform', 'rotate(' + secondsDegrees + 'deg)');
+  // Minutes
+  var minutes = date.getMinutes();
+  var minutesRatio = 60/360;
+  var minutesDegrees = minutes / minutesRatio;
+
+  // Hours
+  var hours = date.getHours();
+  var hoursRatio = 24/360;
+  var hoursDegrees = hours / hoursRatio;
+
+ // Days
+  var day = date.getDay();
+  var dayRatio = 24/360;
+  var dayDegrees = day / dayRatio;
+
+ $('.circle15').css('transform', 'rotate(' + secondsDegrees + 'deg)');
+ $('.circle12').css('transform', 'rotate(' + minutesDegrees + 'deg)');
+ $('.circle9').css('transform', 'rotate(' + hoursDegrees + 'deg)');
+ $('.circle7').css('transform', 'rotate(' + dayDegrees + 'deg)');
 }
+
+
+
+
+
 
 $(function() {
 	updateTime();
@@ -19,21 +42,3 @@ $(function() {
 
 });
 
-// var date = new Date();
-
-// function updateTime() {
-// date = new Date();
-
-// var hours = date.hours();
-
-// var hoursRatio = 24/360;
-// var hoursDegrees = hours / hoursRatio;
-  
-//   $('.circle12').css('transform', 'rotate(' + hoursDegrees + 'deg)');
-// }
-
-// $(function() {
-// 	updateTime();
-// 	setInterval(updateTime, 50);
-
-// });
