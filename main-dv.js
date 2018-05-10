c89829c51852988d8a653df19912df1c104d43b0
Yaren Kaya <yarenkaya@Yarens-Air-3.fios-router.home>
2018-05-10 14:05:43 -0400

    dream vacation

--- main-dv.js
+++ main-dv.js
@@ -1,9 +1,21 @@
 var date = new Date();
+var updateInterval = 50;
+
+$('.date-button').click(function() {
+  var $button = $(this);
+
+  if ( !$button.attr('data-date') ) {
+    console.warn('You must add a data-date attribute to this button to work properly.');
+  }
+
+  date = new Date( Date.parse($button.attr('data-date')) );
+});
 
 function updateTime() {
-  date = new Date();
+  date = new Date(date.getTime() + updateInterval);
+  console.log(date);
    // Ratio is: unit of time / 360 degrees//
-  
+
   // Seconds
   var milliseconds = date.getMilliseconds();
   var millisecondsRatio = 1000/360;
@@ -13,7 +25,7 @@ function updateTime() {
   var seconds = date.getSeconds();
   var secondsRatio = 60/360;
   var secondsDegrees = seconds / secondsRatio;
-  
+
   // Minutes
   var minutes = date.getMinutes();
   var minutesRatio = 60/360;
@@ -40,12 +52,14 @@ function updateTime() {
    var yearDegrees = year /yearRatio;
 
  $('.circle16').css('transform', 'rotate(' + millisecondsDegrees + 'deg)');
- $('.circle14').css('transform', 'rotate(' + millisecondsDegrees + 'deg)');
+ $('.circle1').css('transform', 'rotate(' + millisecondsDegrees + 'deg)');
  $('.circle2').css('transform', 'rotate(' + secondsDegrees + 'deg)');
- $('.circle5').css('transform', 'rotate(' + minutesDegrees + 'deg)');
+ $('.circle3').css('transform', 'rotate(' + secondsDegrees + 'deg)');
+ $('.circle6').css('transform', 'rotate(' + minutesDegrees + 'deg)');
  $('.circle12').css('transform', 'rotate(' + hoursDegrees + 'deg)');
  $('.circle10').css('transform', 'rotate(' + dayDegrees + 'deg)');
  $('.circle13').css('transform', 'rotate(' + monthDegrees + 'deg)');
+ $('.circle15').css('transform', 'rotate(' + yearDegrees + 'deg)');
 }
 
 
@@ -55,9 +69,7 @@ function updateTime() {
 
 $(function() {
 	updateTime();
-	setInterval(updateTime, 50);
+	setInterval(updateTime, updateInterval);
 });
-
-
 
 
